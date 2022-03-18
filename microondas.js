08ad4dcc6cd56df580446e63a93368450f1f68b3
Jonathas Arruda <jonathasarruda@outlook.com>
2022-03-18 13:39:11 -0300

    Adding firt test on code

--- microondas.js
+++ microondas.js
@@ -7,29 +7,38 @@ let comidas = [
 ];
 
 function microondas(prato, tempo) {
-  if (prato >0 && prato <5) {
-    let padrao = comidas[prato - 1].tempo
-    let bip ="\nPrato pronto, bom apetite!!!"
-    let texto=`Prato selecionado foi: ${comidas[prato - 1].prato}\n`
-  
-    if (tempo < padrao) {
-      return `${texto}Tempo insuficiente ${bip}`;
-    } else if (tempo == padrao) {
-      return `${texto}Tempo padrao selecionado ${bip}`;
-    } else if (tempo < padrao * 2) {
-      return `${texto}Potencia alterada com sucesso! ${bip}`;
-    } else if (tempo < padrao * 3) {
-      return `${texto}A comida queimou ${bip}`;
+  let param1 = Number.isInteger(prato);
+  let param2 = Number.isInteger(tempo);
+
+  param1 == false
+    ? console.log("Prato indefinido, por favor digite um caracter valido!")
+    : param2 == false
+    ? console.log("Tempo indefinido, por favor digite um caracter valido!")
+    : "";
+
+  if (param1 && param2) {
+    if ((prato > 0 && prato < 5) || params) {
+      let padrao = comidas[prato - 1].tempo;
+      let bip = "\nPrato pronto, bom apetite!!!";
+      let texto = `Prato selecionado foi: ${comidas[prato - 1].prato}\n`;
+
+      if (tempo < padrao) {
+        return `${texto}Tempo insuficiente ${bip}`;
+      } else if (tempo == padrao) {
+        return `${texto}Tempo padrao selecionado ${bip}`;
+      } else if (tempo < padrao * 2) {
+        return `${texto}Potencia alterada com sucesso! ${bip}`;
+      } else if (tempo < padrao * 3) {
+        return `${texto}A comida queimou ${bip}`;
+      } else {
+        return `${texto}Kabumm ${bip}`;
+      }
     } else {
-      return `${texto}Kabumm ${bip}`;
+      return "O número digitado não foi encontrado dentre as opções do MENU!";
     }
-
   }else{
-    return "O número digitado não foi encontrado dentre as opções do MENU!"
-
+    return ""
   }
-
-  
 }
 
 //MENU
@@ -41,4 +50,8 @@ function microondas(prato, tempo) {
 
 //De acordo com o menu, use a função microondas().
 //Digite primeiro o numero respectivo ao prato escolhido, e a quantidade de tempo em segundos:
-console.log(microondas(1,15));
+console.log(microondas("a", 1));
+
+//
+// + console.log("Prato indefinido, por favor digite um caracter valido"
+// + console.log("Tempo indefinido, por favor digite um caracter valido"
